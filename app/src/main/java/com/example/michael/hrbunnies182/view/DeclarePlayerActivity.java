2447a920e2b9412a3eeb9a6d3990046d5b81dcfb
Charbunny <char_bunny@hotmail.com>
2016-01-17 05:04:46 -0600

    Fixed px to dp for choose players

--- app/src/main/java/com/example/michael/hrbunnies182/view/DeclarePlayerActivity.java
+++ app/src/main/java/com/example/michael/hrbunnies182/view/DeclarePlayerActivity.java
@@ -36,13 +36,24 @@ public class DeclarePlayerActivity extends AppCompatActivity {
         TextView playerTitle = (TextView) findViewById(R.id.toolbar_title_player_name);
         playerTitle.setText(newColorName + " Player");
 
-        TextView playerName = (TextView) findViewById(R.id.textViewPlayerName);
+        Button playerName = (Button) findViewById(R.id.textViewPlayerName);
         playerName.setBackgroundResource(getBackgroundColor(player.getColor()));
         playerName.setText(newColorName);
 
 
         Button thatsMe = (Button) findViewById(R.id.buttonMe);
         thatsMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent drawCardsActivity = new Intent(DeclarePlayerActivity.this, com.example.michael.hrbunnies182.view.DrawCardsKeepSomeActivity.class);
+                drawCardsActivity.putExtra("player_num", playerIndex);
+                drawCardsActivity.putExtra("card_min", 2);
+                drawCardsActivity.putExtra("next_step_buttons", (Serializable) Collections.singletonMap("Continue", new ViewHandActivity.InitializeNextPlayer((playerIndex + 1))));
+                startActivity(drawCardsActivity);
+            }
+        });
+
+        playerName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent drawCardsActivity = new Intent(DeclarePlayerActivity.this, com.example.michael.hrbunnies182.view.DrawCardsKeepSomeActivity.class);
